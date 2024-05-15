141526f86bd27ef10252f2ffbf0223cb778e6100
oerif <asdfuhi>
2024-05-15 21:25:09 +0900

    bisect

--- abc353/abc353_c.py
+++ abc353/abc353_c.py
@@ -1,6 +1,9 @@
 # 和はlist内の2要素のすべての組み合わせを出せばいい。
 # それぞれの要素は、n-1回ずつ出てくるので、全ての和とn-1の積になる。
 
+import bisect
+
+
 n = int(input())
 a_list = sorted(list(map(int, input().split())))
 
@@ -10,18 +13,28 @@ sum_all = a_sum * (n - 1)
 # ただし、設問は10**8 を超えている場合余りを求めているので、
 # 10**8を超える組み合わせの数を求める必要がある。
 
+# わざわざ自前で二分探索を書く必要はない？
+# def count_pairs_exceeding_limit(a_list, limit=10**8):
+#     count = 0
+#     n = len(a_list)
+#     for i in range(n):
+#         left, right = i + 1, n - 1
+#         while left <= right:
+#             mid = (left + right) // 2
+#             if a_list[i] + a_list[mid] >= limit:
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
+#         count += n - left
+#     return count
 
+
+# こうすればいいはず。
 def count_pairs_exceeding_limit(a_list, limit=10**8):
     count = 0
     n = len(a_list)
     for i in range(n):
-        left, right = i + 1, n - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if a_list[i] + a_list[mid] >= limit:
-                right = mid - 1
-            else:
-                left = mid + 1
+        left = bisect.bisect_left(a_list, limit - a_list[i], i + 1)
         count += n - left
     return count
 
